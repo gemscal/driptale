9ae67139f0c80226ecfae027b90385e80999480a
James Carl Junio <jamescarljunio@gmail.com>
2026-01-26 21:06:49 +0800

    feat: added rate limiter

--- driptale/main.py
+++ driptale/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from driptale.health.endpoints import router as health_router
+from driptale.rate_limiter import limiter, setup_rate_limiting
 
 app = FastAPI()
+setup_rate_limiting(app)
 
 
 @app.get("/")
 def read_root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/testLimit")
+@limiter.limit("5/minute")
+def test_limit(request: Request):
     return {"message": "Hello, World!"}
 
 
